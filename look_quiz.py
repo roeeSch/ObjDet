dcfdd3b74aa6242582f571d157b4b4d44353a5b9
LAPROEE\ROEE <roee.schmidt@gmail.com>
2018-07-01 18:30:25 +0300

    added if __name__ == "__main__"

--- look_quiz.py
+++ look_quiz.py
@@ -38,28 +38,28 @@ def data_look(car_list, notcar_list):
     # Return data_dict
     return data_dict
 
+if __name__=="__main__":
+    data_info = data_look(cars, notcars)
 
-data_info = data_look(cars, notcars)
+    print('Your function returned a count of',
+          data_info["n_cars"], ' cars and',
+          data_info["n_notcars"], ' non-cars')
+    print('of size: ', data_info["image_shape"], ' and data type:',
+          data_info["data_type"])
+    # Just for fun choose random car / not-car indices and plot example images
+    car_ind = np.random.randint(0, len(cars))
+    notcar_ind = np.random.randint(0, len(notcars))
 
-print('Your function returned a count of',
-      data_info["n_cars"], ' cars and',
-      data_info["n_notcars"], ' non-cars')
-print('of size: ', data_info["image_shape"], ' and data type:',
-      data_info["data_type"])
-# Just for fun choose random car / not-car indices and plot example images
-car_ind = np.random.randint(0, len(cars))
-notcar_ind = np.random.randint(0, len(notcars))
+    # Read in car / not-car images
+    car_image = mpimg.imread(cars[car_ind])
+    notcar_image = mpimg.imread(notcars[notcar_ind])
 
-# Read in car / not-car images
-car_image = mpimg.imread(cars[car_ind])
-notcar_image = mpimg.imread(notcars[notcar_ind])
-
-# Plot the examples
-fig = plt.figure()
-plt.subplot(121)
-plt.imshow(car_image)
-plt.title('Example Car Image')
-plt.subplot(122)
-plt.imshow(notcar_image)
-plt.title('Example Not-car Image')
-plt.show()
+    # Plot the examples
+    fig = plt.figure()
+    plt.subplot(121)
+    plt.imshow(car_image)
+    plt.title('Example Car Image')
+    plt.subplot(122)
+    plt.imshow(notcar_image)
+    plt.title('Example Not-car Image')
+    plt.show()
